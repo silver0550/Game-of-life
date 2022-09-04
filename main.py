1a8a7b0c8f68d46a7e3d8ace570511eb5f32d631
silver0550 <g.silver.014@gmail.com>
2022-09-04 13:03:11 +0200

    refactoring, debugging add __doc__'s

--- main.py
+++ main.py
@@ -7,71 +7,89 @@ from time import sleep
 
 
 class Gofengine:
+    """ The engine of Game of Life. """
 
     def __init__(self):
         self._place = (np.zeros((100, 100))) != 0
 
-    def show(self):
-        for row in self._place:
+    def show(self):                                 # not used because it has a GUI
+        """ Print the world to the console """
+
+        for row in self.place:
             for column in row:
                 print('O', end='') if column else print('.', end='')
             print()
         print()
 
     def count_neighbor(self, row, column):
+        """ Return the number of neighbours
+            Input: row: int, column: int
+            Output: int """
 
-        count = self._place[row-1:row+2, column-1:column+2].sum()
-        if self._place[row][column]: count -= 1
+        count = self.place[row - 1: row + 2, column - 1: column + 2].sum()
+        if self.place[row][column]: count -= 1
 
         return count
 
     def update_data(self):
+        """ Play 1 life cycle and update the world"""
 
-        new_place = np.copy(self._place)
+        new_place = np.copy(self.place)
 
-        self.valid_rows = {i + j for i in sorted(set(self.life[0])) for j in [-1, 0, 1]}
-        self.valid_columns = {i + j for i in sorted(set(self.life[1])) for j in [-1, 0, 1]}
+        valid_rows = {i + j for i in sorted(set(self.life[0])) for j in [-1, 0, 1]}
+        valid_columns = {i + j for i in sorted(set(self.life[1])) for j in [-1, 0, 1]}
 
-        for row in self.valid_rows:
-            for column in self.valid_columns:
+        for row in valid_rows:
+            for column in valid_columns:
                 count = self.count_neighbor(row, column)
                 if count == 3: new_place[row][column] = True
                 elif count < 2 or count > 3: new_place[row][column] = False
 
-        self._place = new_place
+        self.place = new_place
 
     def is_empty(self):
-        if not self._place.sum(): return True
+        if not self.place.sum(): return True
         return False
 
-    def run(self): # nincs rá szükség
-        while True:
+    def run(self):                                  # not used because it has a GUI
+        """Run the game in the console """
 
+        while True:
             self.resize_place()
             self.show()
             if self.is_empty(): break
             self.update_data()
-            sleep(0.5)
+            sleep(0.2)
 
     def resize_place(self):
-        if min(self.life[0]) == 1: self._place = np.insert(self._place, 0, 0, axis=0)
-        if min(self.life[1]) == 1: self._place = np.insert(self._place, 0, 0, axis=1)
+        """ When life closes to the limit, expand the world """
 
-        if max(self.life[0]) == self._place.shape[0] - 1: self._place = np.insert(self._place, self._place.shape[0], 0, axis=0)
-        if max(self.life[1]) == self._place.shape[1] - 1: self._place = np.insert(self._place, self._place.shape[1], 0, axis=1)
+        if min(self.life[0]) < 2: self.place = np.insert(self.place, 0, False, axis=0)
+        if min(self.life[1]) < 2: self.place = np.insert(self.place, 0, False, axis=1)
+
+        if max(self.life[0]) == self.place.shape[0] - 1: self.place = np.insert(self.place, self.place.shape[0], False, axis=0)
+        if max(self.life[1]) == self.place.shape[1] - 1: self.place = np.insert(self.place, self.place.shape[1], False, axis=1)
 
 
     @property
+    def place(self):
+        return self._place
+
+    @place.setter
+    def place(self, new_place):
+        self._place = new_place
+
+    @property
     def life(self):
-        return np.where(self._place)
+        return np.where(self.place)
 
     @life.setter
-    def life(self, cordinates):
-        #TODO: input check
-        self._place[cordinates[0], cordinates[1]] = True
+    def life(self, coordinates):
+        if coordinates[0] and coordinates[1]: self.place[coordinates[0], coordinates[1]] = True
 
 
 class Gameoflife(Gofengine):
+    """ An application is to animate the game of life"""
 
     def __init__(self):
         Gofengine.__init__(self)
@@ -81,23 +99,13 @@ class Gameoflife(Gofengine):
         self._root.resizable(False, False)
 
         self._pause = False
+        self._round = 0
 
         self._world = tk.LabelFrame(self._root, text= 'World', height=880, width=1000)
         self._world.place(relx=0.01, rely=0.01)
 
-        self._game_place = tk.Canvas(self._world, background='lightgray')
+        self._game_place = tk.Canvas(self._world, height=10000, width=1000, background='lightgray')
         self._game_place.place(relheight=1, relwidth=1)
-
-        # self._vizszint = tk.Scrollbar(self._game_place)
-        # self._vizszint.pack(side=tk.RIGHT, fill=tk.Y)
-        #
-        # t = tk.Text(self._game_place, width=500, height=200, wrap=tk.NONE, yscrollcommand=self._vizszint.set)
-        # [t.insert(tk.END, "this is some text\n") for _ in range(20)]
-        #
-        # t.pack(side=tk.TOP, fill= tk.X)
-        #
-        # self._vizszint.config(command=t.yview)
-
 
         self._status = tk.LabelFrame(self._root, height=80, width=150, text='Status')
         self._status.place(relx=0.86, rely=0.01)
@@ -109,22 +117,30 @@ class Gameoflife(Gofengine):
 
         tk.Button(self._control, width=11, font=('Arial', 15), text='Play', command=self.run_app).place(relx=0.04, rely=0.05)
         tk.Button(self._control, width=11, font=('Arial', 15), text='Pause', command=self.pause).place(relx=0.04, rely=0.3)
-        tk.Button(self._control, width=11, font=('Arial', 15), text='Abort').place(relx=0.04, rely=0.55)
+        tk.Button(self._control, width=11, font=('Arial', 15), text='Kill all', command=self.kill).place(relx=0.04, rely=0.55)
         tk.Button(self._control, width=11, font=('Arial', 15), text='Exit', command=self._root.destroy).place(relx=0.04, rely=0.8)
 
         self._number_of_round = tk.LabelFrame(self._root, height=100, width=150, text='Round')
         self._number_of_round.place(relx=0.86, rely=0.75)
-        self._number_of_round_text = tk.Label(self._number_of_round, font=('Arial', 20), text='0')
+        self._number_of_round_text = tk.Label(self._number_of_round, font=('Arial', 20), text= 0)
         self._number_of_round_text.place(relx=0.2, rely=0.25)
 
         self._number_of_life = tk.LabelFrame(self._root, height=100, width=150, text='Population')
         self._number_of_life.place(relx=0.86, rely=0.875)
-        self._number_of_life_text = tk.Label(self._number_of_life, font=('Arial', 20), text='0')
+        self._number_of_life_text = tk.Label(self._number_of_life, font=('Arial', 20), text= 0)
         self._number_of_life_text.place(relx=0.2, rely=0.25)
 
         self._game_place.bind('<Button>', self.add_cell)
 
+    def kill(self):
+        """ Reset the place"""
+        self.place = (np.zeros((100, 100))) != 0
+        self._round = 0
+        self._status_text.config(text='Extinction')
+        self._status_text.update()
+
     def pause(self):
+
         self._pause = True
 
     def add_cell(self, event):
@@ -144,19 +160,22 @@ class Gameoflife(Gofengine):
         self._game_place.update()
 
     def run_app(self):
+        """ Run the game in the interface """
         self._pause = False
-        self.round = 0
+
         while True:
-            self.round += 1
+            self._round += 1
             if not self._place.sum(): break
+
             if self._pause:
                 self._status_text.config(text='Pause')
                 self._status_text.update()
                 break
+
             self.update_app()
             self.update_data()
 
-            self._number_of_round_text.config(text=self.round)
+            self._number_of_round_text.config(text=self._round)
             self._number_of_round_text.update()
 
             self._number_of_life_text.config(text=self._place.sum())
@@ -166,10 +185,14 @@ class Gameoflife(Gofengine):
             self._status_text.update()
 
             if self.is_empty():
-                messagebox.showinfo(title="End", message="A teljes populáció kihalt!")
-                self._status_text.config(text='Stop')
+
+                self._round = 0
+                self._status_text.config(text='Extinction')
                 self._status_text.update()
+
+                messagebox.showinfo(title="End", message="The life is over")
                 break
+
             sleep(0.1)
 
 
